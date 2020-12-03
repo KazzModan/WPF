fba2d10e8131c4871cc16f2d9295590ce0c15a6c
KazzModan <kklopov64@gmail.com>
2020-12-03 23:56:26 +0200

    Added Bindings

---
+++ binds/task01/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace task01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    { 
+            ViewModel model = new ViewModel();        
+        public MainWindow()
+        {
+            model.SelectedColor = new MyColor();
+        InitializeComponent();
+            lBox.ItemsSource = model.colors;
+            this.DataContext = model;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            model.Add();
+        }
+        private void lBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            model.BorderColor.A = model.colors[lBox.SelectedIndex].A;
+            model.BorderColor.R = model.colors[lBox.SelectedIndex].R;
+            model.BorderColor.G = model.colors[lBox.SelectedIndex].G;
+            model.BorderColor.B = model.colors[lBox.SelectedIndex].B;
+        }
+    }
+}
---
+++ binds/task01/ViewModel.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace task01
+{
+    public class ViewModel
+    {
+        public ObservableCollection<MyColor> colors = new ObservableCollection<MyColor>();
+        
+        public MyColor SelectedColor { get; set; }
+        public MyColor BorderColor { get; set; }
+
+        public void Add()
+        {
+            SelectedColor = (MyColor)BorderColor.Clone();
+            colors.Add(SelectedColor);
+            BorderColor.A = 0;
+            BorderColor.R = 0;
+            BorderColor.G = 0;
+            BorderColor.B = 0;
+        }
+          public void Remove()
+        {
+            
+        }
+        public ViewModel()
+        {
+            SelectedColor = new MyColor();
+            BorderColor = new MyColor();
+
+        }
+    }
+   public class MyColor : INotifyPropertyChanged , ICloneable 
+    {
+        private byte a;
+
+        public MyColor()
+        {
+         
+        }
+        public byte A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                a = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Color));
+            }
+        }
+        private byte r;
+
+        public byte R
+        {
+            get
+            {
+                return r;
+            }
+            set
+            {
+                r = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Color));
+            }
+        }
+
+        private byte g;
+
+        public byte G
+        {
+            get
+            {
+                return g;
+            }
+            set
+            {
+                g = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Color));
+            }
+        }
+        private byte b;
+
+        public byte B
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                b = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Color));
+            }
+        }
+      
+        public Color Color => Color.FromArgb(A, R, G, B);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public object Clone()
+        {
+            MyColor clone =  (MyColor)this.MemberwiseClone();
+            return clone;
+        }
+    }
+       
+    
+}
